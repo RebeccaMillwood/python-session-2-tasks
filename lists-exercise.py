ca193b8bdd2386bf87a42fd0a559b75381749671
RebeccaMillwood <bec-millwood@live.com.au>
2020-07-23 23:43:39 +1000

    amended Q2 exercise 1, and programmed Q2 exercise 2

--- lists-exercise.py
+++ lists-exercise.py
@@ -24,18 +24,21 @@
 
 # Question 2:
 
-# mailing_list = [
-#     ["Roary", "roary@moth.catchers"],
-#     ["Remus", "remus@kapers.dog"],
-#     ["Prince Thomas of Whitepaw", "hrh.thomas@royalty.wp"], 
-#     ["Biscuit", "biscuit@whippies.park"],
-#     ["Rory", "rory@whippies.park"],
-# ]
+mailing_list = [
+    ["Roary", "roary@moth.catchers"],
+    ["Remus", "remus@kapers.dog"],
+    ["Prince Thomas of Whitepaw", "hrh.thomas@royalty.wp"], 
+    ["Biscuit", "biscuit@whippies.park"],
+    ["Rory", "rory@whippies.park"],
+]
 
 # # print(mailing_list)
 
-# for details in mailing_list:
-#     print(f"{details[0]}: {details[1]}")
+print(f"{mailing_list[0][0]}: {mailing_list[0][1]}")
+print(f"{mailing_list[1][0]}: {mailing_list[1][1]}")
+print(f"{mailing_list[2][0]}: {mailing_list[2][1]}")
+print(f"{mailing_list[3][0]}: {mailing_list[3][1]}")
+print(f"{mailing_list[4][0]}: {mailing_list[4][1]}")
  
 
 # Question 3:
@@ -54,13 +57,13 @@
 
 # Question 4
 
-string = input("enter a short sentence: ")
-sentence = []
-sentence = string.split()
-print(sentence)
+# string = input("enter a short sentence: ")
+# sentence = []
+# sentence = string.split()
+# print(sentence)
 
-characterlist = list(string)
-print(characterlist)
+# characterlist = list(string)
+# print(characterlist)
 
-print(f"{len(sentence)} {sentence}")
-print(f"{len(characterlist)} {characterlist}")
+# print(f"{len(sentence)} {sentence}")
+# print(f"{len(characterlist)} {characterlist}")
--- loops-exercise.py
+++ loops-exercise.py
@@ -1,1 +1,20 @@
 # Question 1
+
+# number = (input("Please enter a number: ")
+  
+
+
+# Question 2
+mailing_list = [
+    ["Roary", "roary@moth.catchers"],
+    ["Remus", "remus@kapers.dog"],
+    ["Prince Thomas of Whitepaw", "hrh.thomas@royalty.wp"], 
+    ["Biscuit", "biscuit@whippies.park"],
+    ["Rory", "rory@whippies.park"],
+]
+
+# print(mailing_list)
+
+for details in mailing_list:
+    print(f"{details[0]}: {details[1]}")
+
