4cd65d96cfe40172df3805591d7c650d31628171
RebeccaMillwood <bec-millwood@live.com.au>
2020-07-23 21:11:01 +1000

    added lists-exercises file

---
+++ lists-exercise.py
@@ -0,0 +1,18 @@
+foods = [
+"orange",
+"apple",
+"banana",
+"strawberry",
+"grape",
+"blueberry",
+["carrot", "cauliflower", "pumpkin"],
+"passionfruit",
+"mango",
+"kiwifruit"
+]
+
+# print(foods)
+
+print(foods[0])
+print(foods[2])
+print(foods[-1])
