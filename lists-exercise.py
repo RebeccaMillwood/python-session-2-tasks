a9199b63b81fb433ab73157785853f104da548ff
RebeccaMillwood <bec-millwood@live.com.au>
2020-07-23 22:23:17 +1000

    programmed question 3 for exercise 1

--- lists-exercise.py
+++ lists-exercise.py
@@ -24,17 +24,30 @@
 
 # Question 2:
 
-mailing_list = [
-    ["Roary", "roary@moth.catchers"],
-    ["Remus", "remus@kapers.dog"],
-    ["Prince Thomas of Whitepaw", "hrh.thomas@royalty.wp"], 
-    ["Biscuit", "biscuit@whippies.park"],
-    ["Rory", "rory@whippies.park"],
-]
+# mailing_list = [
+#     ["Roary", "roary@moth.catchers"],
+#     ["Remus", "remus@kapers.dog"],
+#     ["Prince Thomas of Whitepaw", "hrh.thomas@royalty.wp"], 
+#     ["Biscuit", "biscuit@whippies.park"],
+#     ["Rory", "rory@whippies.park"],
+# ]
 
-# print(mailing_list)
+# # print(mailing_list)
 
-for details in mailing_list:
-    print(f"{details[0]}: {details[1]}")
+# for details in mailing_list:
+#     print(f"{details[0]}: {details[1]}")
  
 
+# Question 3:
+
+name1 = input("Enter your name ")
+name2 = input("Enter another name ")
+name3 = input("Enter a final name ")
+
+list_of_names = [
+    name1,
+    name2,
+    name3,
+]
+
+print(list_of_names)
