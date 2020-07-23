8c051d349a2c18fb2f3723ab1385b5e38451344a
RebeccaMillwood <bec-millwood@live.com.au>
2020-07-23 23:51:23 +1000

    added notes for questions 1, 3 and 4 for exercise 2

--- loops-exercise.py
+++ loops-exercise.py
@@ -1,20 +1,32 @@
 # Question 1
+# continuously ask the user to enter a number until they provide a blank input
+# output the sum of all the numbers
 
 # number = (input("Please enter a number: ")
   
 
 
 # Question 2
-mailing_list = [
-    ["Roary", "roary@moth.catchers"],
-    ["Remus", "remus@kapers.dog"],
-    ["Prince Thomas of Whitepaw", "hrh.thomas@royalty.wp"], 
-    ["Biscuit", "biscuit@whippies.park"],
-    ["Rory", "rory@whippies.park"],
-]
+
+# mailing_list = [
+#     ["Roary", "roary@moth.catchers"],
+#     ["Remus", "remus@kapers.dog"],
+#     ["Prince Thomas of Whitepaw", "hrh.thomas@royalty.wp"], 
+#     ["Biscuit", "biscuit@whippies.park"],
+#     ["Rory", "rory@whippies.park"],
+# ]
 
 # print(mailing_list)
 
-for details in mailing_list:
-    print(f"{details[0]}: {details[1]}")
+# for details in mailing_list:
+#     print(f"{details[0]}: {details[1]}")
+
+
+# Question 3
+# use a while loop to ask the user for 3 names and append them to a list
+# then use a for loop to print the list
+
+# name = input("What is your name? ")
+
+# Question 4
 
