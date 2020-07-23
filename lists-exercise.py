36634d328042fdf05279e05c8f6472e0d3dcdbc8
RebeccaMillwood <bec-millwood@live.com.au>
2020-07-23 22:00:32 +1000

    programmed question 2 for exercise 1

--- lists-exercise.py
+++ lists-exercise.py
@@ -1,21 +1,40 @@
-foods = [
-"orange",
-"apple",
-"banana",
-"strawberry",
-"grape",
-"blueberry",
-["carrot", "cauliflower", "pumpkin"],
-"passionfruit",
-"mango",
-"kiwifruit"
+# Question 1:
+
+# foods = [
+# "orange",
+# "apple",
+# "banana",
+# "strawberry",
+# "grape",
+# "blueberry",
+# ["carrot", "cauliflower", "pumpkin"],
+# "passionfruit",
+# "mango",
+# "kiwifruit"
+# ]
+
+# # print(foods)
+
+# print(foods[0])
+# print(foods[2])
+# print(foods[-1])
+# print(foods[0:3])
+# print(foods[7:10])
+# print(foods[6][-1])
+
+# Question 2:
+
+mailing_list = [
+    ["Roary", "roary@moth.catchers"],
+    ["Remus", "remus@kapers.dog"],
+    ["Prince Thomas of Whitepaw", "hrh.thomas@royalty.wp"], 
+    ["Biscuit", "biscuit@whippies.park"],
+    ["Rory", "rory@whippies.park"],
 ]
 
-# print(foods)
+# print(mailing_list)
 
-print(foods[0])
-print(foods[2])
-print(foods[-1])
-print(foods[0:3])
-print(foods[7:10])
-print(foods[6][-1])
+for details in mailing_list:
+    print(f"{details[0]}: {details[1]}")
+ 
+
