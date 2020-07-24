bfab2ddbc8b9ce27536d823590c5ce0b130af99f
RebeccaMillwood <bec-millwood@live.com.au>
2020-07-24 22:56:18 +1000

    testing code for question 1 and programmed answer for question 3

--- loops-exercise.py
+++ loops-exercise.py
@@ -2,9 +2,38 @@
 # continuously ask the user to enter a number until they provide a blank input
 # output the sum of all the numbers
 
-# number = (input("Please enter a number: ")
-  
 
+# number = (input("Please enter a number: ")
+# while len(number) > 1:
+#     if number => 1:
+#     int(number) 
+#     else:
+#         print
+
+# numbers = []
+
+# new_numbers = input("Please enter a number: ")
+# while int(new_numbers) > 1:
+#     numbers.append(new_numbers)
+#     new_numbers = (input("Please enter a number: "))
+#     result = (sum(new_numbers))
+#     print(result)
+
+# number = input("Enter a number ")
+# while len(number) > 1:
+#     if number > 1:
+#         print ("test working")
+#     else:
+#         print("try again")
+#     number = input("Enter a number")   
+
+# numbers = []
+# number = int(input("Enter a number: "))
+# while int(number) > 0:
+#     if number =>5:
+#         numbers = int(input('Enter number '))
+#         numbers.append(number)
+#         print(sum(lst))
 
 # Question 2
 
@@ -26,7 +55,40 @@
 # use a while loop to ask the user for 3 names and append them to a list
 # then use a for loop to print the list
 
-# name = input("What is your name? ")
+# numbers = []
+# number = int(input("Enter a number: "))
+# while int(number) > 0:
+#     if number =>5:
+#         numbers = int(input('Enter number '))
+#         numbers.append(number)
+#         print(sum(lst))
+
+names = []
+count = 0
+while count < 3:
+    name = input("What is your name? ")
+    names.append(name)
+    count += 1
+    print()
+
+for name in names:
+    print(name)
+
+# count = 0
+# while count < 3:
+#     name = input("What is your name? ")
+#     print(name)
+#     count += 1
+
+# # list_name = []
+# # for name in name:
+# #     list_name.append(name)
+# #     print(list_name)
+
+# list_name = [
+#     name
+# ]
+
 
 # Question 4
 
