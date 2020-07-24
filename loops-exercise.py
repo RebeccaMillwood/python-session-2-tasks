b375c67107a573de8960cd5d1c5a5a83404cec47
RebeccaMillwood <bec-millwood@live.com.au>
2020-07-25 00:28:58 +1000

    programmed answers to all questions for exercise 2

--- loops-exercise.py
+++ loops-exercise.py
@@ -3,6 +3,17 @@
 # output the sum of all the numbers
 
 
+new_numbers = []
+
+number = (input("Please enter a number: "))
+
+while number:
+        new_numbers.append(int(number))
+        number = (input("Please enter a number: ")) 
+total = sum(new_numbers)
+print(total)  
+
+
 # number = (input("Please enter a number: ")
 # while len(number) > 1:
 #     if number => 1:
@@ -55,40 +66,39 @@
 # use a while loop to ask the user for 3 names and append them to a list
 # then use a for loop to print the list
 
-# numbers = []
-# number = int(input("Enter a number: "))
-# while int(number) > 0:
-#     if number =>5:
-#         numbers = int(input('Enter number '))
-#         numbers.append(number)
-#         print(sum(lst))
 
-names = []
-count = 0
-while count < 3:
-    name = input("What is your name? ")
-    names.append(name)
-    count += 1
-    print()
-
-for name in names:
-    print(name)
-
+# names = []
 # count = 0
 # while count < 3:
 #     name = input("What is your name? ")
-#     print(name)
+#     names.append(name)
 #     count += 1
+#     print()
 
-# # list_name = []
-# # for name in name:
-# #     list_name.append(name)
-# #     print(list_name)
-
-# list_name = [
-#     name
-# ]
+# for name in names:
+#     print(name)
 
 
 # Question 4
 
+# groceries = [
+#     ["Baby Spinach", 2.78],
+#     ["Hot Chocolate", 3.70],
+#     ["Crackers", 2.10],
+#     ["Bacon", 9.00],
+#     ["Carrots", 0.56],
+#     ["Oranges", 3.08]
+# ]
+
+# total = 0
+
+# for item in groceries:
+#     quantity = input(f"How many {item[0]} did you buy? ")
+#     item[1] = item[1] * int(quantity)
+#     total += item[1]
+
+# print(f"====Izzy's Food Emporium====")
+# for item in groceries:
+#     print(f"{item[0]:<20} ${item[1]:.2f}")
+# print(f"${total:>25.2f}")
+# print(f"============================")
